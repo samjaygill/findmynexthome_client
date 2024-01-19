0ec45f9ddc0d03bdf7c7500fef13f388538cb6e5
samjaygill <138401809+samjaygill@users.noreply.github.com>
2024-01-19 19:30:11 +0000

    styling updates

--- src/components/MeetTheTeam.jsx
+++ src/components/MeetTheTeam.jsx
@@ -3,85 +3,92 @@ import "./css/MeetTheTeam.css";
 
 function MeetTheTeam() {
   return (
-    <div className="container">
-      <div className="card">
-          <img alt="staff" src="https://i.ibb.co/Nx4Qpwx/me.jpg" className="team-img" />
+    <>
+      <h3 className="heading">Meet the team:</h3>
+      <div className="container">
+        <div className="card">
+          <img
+            alt="staff"
+            src="https://i.ibb.co/Nx4Qpwx/me.jpg"
+            className="team-img"
+          />
           <h3 className="name">
             Samantha Gillies
             <br />
             <small className="title">Director</small>
           </h3>
+        </div>
 
-      </div>
-
-      <div className="card">
+        <div className="card">
           <img
             alt="staff"
-            src="https://www.kycolonels.org/wp-content/uploads/2019/12/BARACK-OBAMA.png" className="team-img"
+            src="https://www.kycolonels.org/wp-content/uploads/2019/12/BARACK-OBAMA.png"
+            className="team-img"
           />
           <h3 className="name">
             Barack Obama
             <br />
             <small className="title">Manager</small>
           </h3>
-      </div>
+        </div>
 
-      <div className="card">
+        <div className="card">
           <img
             alt="staff"
-            src="https://media1.popsugar-assets.com/files/thumbor/GwrkdTP4PmYn0v4dBkX1ulwT83s/1196x242:2764x1810/fit-in/500x500/filters:format_auto-!!-:strip_icc-!!-/2019/09/04/006/n/1922398/fe7006935d7044366c8982.50946989_/i/Rihanna.jpg" className="team-img"
+            src="https://media1.popsugar-assets.com/files/thumbor/GwrkdTP4PmYn0v4dBkX1ulwT83s/1196x242:2764x1810/fit-in/500x500/filters:format_auto-!!-:strip_icc-!!-/2019/09/04/006/n/1922398/fe7006935d7044366c8982.50946989_/i/Rihanna.jpg"
+            className="team-img"
           />
           <h3 className="name">
             Rihanna
             <br />
             <small className="title">Company Accountant</small>
           </h3>
-      </div>
+        </div>
 
-      <div className="card">
+        <div className="card">
           <img
             alt="staff"
-            src="https://i.pinimg.com/1200x/9d/d7/38/9dd738872e3a3aa72204128332c9fd13.jpg" className="team-img"
+            src="https://i.pinimg.com/1200x/9d/d7/38/9dd738872e3a3aa72204128332c9fd13.jpg"
+            className="team-img"
           />
           <h3 className="name">
             Brad Pitt
             <br />
             <small className="title">Trusted Property Valuer</small>
           </h3>
+        </div>
 
-      </div>
-
-      <div className="card">
+        <div className="card">
           <img
             alt="staff"
-            src="https://media1.popsugar-assets.com/files/thumbor/0U-zybOekQCbQb3CcjS2JG9J_iA/0x25:2579x2604/fit-in/500x500/filters:format_auto-!!-:strip_icc-!!-/2020/04/27/733/n/1922398/780a84fa5ea709d4e4fb02.25825476_/i/Avril-Lavigne.jpg" className="team-img"
+            src="https://media1.popsugar-assets.com/files/thumbor/0U-zybOekQCbQb3CcjS2JG9J_iA/0x25:2579x2604/fit-in/500x500/filters:format_auto-!!-:strip_icc-!!-/2020/04/27/733/n/1922398/780a84fa5ea709d4e4fb02.25825476_/i/Avril-Lavigne.jpg"
+            className="team-img"
           />
           <h3 className="name">
             Avril Lavigne
             <br />
             <small className="title">Trusted Property Advisor</small>
           </h3>
-      </div>
+        </div>
 
-      <div className="card">
-
+        <div className="card">
           <img
             alt="staff"
-            src="https://media1.popsugar-assets.com/files/thumbor/m5hWnoLb2swg0qdQ9HwqhNmDR4s/616x88:2931x2403/fit-in/500x500/filters:format_auto-!!-:strip_icc-!!-/2020/02/24/871/n/1922398/a66872b45e542a05d1cd50.97536705_/i/Justin-Bieber.jpg" className="team-img"
+            src="https://media1.popsugar-assets.com/files/thumbor/m5hWnoLb2swg0qdQ9HwqhNmDR4s/616x88:2931x2403/fit-in/500x500/filters:format_auto-!!-:strip_icc-!!-/2020/02/24/871/n/1922398/a66872b45e542a05d1cd50.97536705_/i/Justin-Bieber.jpg"
+            className="team-img"
           />
           <h3 className="name">
             Justin Bieber
             <br />
             <small className="title">Trusted Property Viewing Agent</small>
           </h3>
+        </div>
 
-      </div>
-
-      <div className="card">
-
+        <div className="card">
           <img
             alt="staff"
-            src="https://media1.popsugar-assets.com/files/thumbor/kBpr8O_kua_hLXAim9CRY6xLELA/0x0:2832x2832/fit-in/500x500/filters:format_auto-!!-:strip_icc-!!-/2019/08/28/845/n/1922398/b5fe4b225d66d3402e0a15.18451932_/i/Ryan-Gosling.jpg" className="team-img"
+            src="https://media1.popsugar-assets.com/files/thumbor/kBpr8O_kua_hLXAim9CRY6xLELA/0x0:2832x2832/fit-in/500x500/filters:format_auto-!!-:strip_icc-!!-/2019/08/28/845/n/1922398/b5fe4b225d66d3402e0a15.18451932_/i/Ryan-Gosling.jpg"
+            className="team-img"
           />
           <h3 className="name">
             Ryan Gosling
@@ -89,8 +96,8 @@ function MeetTheTeam() {
             <small className="title">Trusted Property Administrator</small>
           </h3>
         </div>
-
-    </div>
+      </div>
+    </>
   );
 }
 
