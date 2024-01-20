1363f23872c652d5d7d11cb8daec58c38bd5f84c
samjaygill <138401809+samjaygill@users.noreply.github.com>
2024-01-20 14:28:04 +0000

    updated blog

--- src/components/ForSale.jsx
+++ src/components/ForSale.jsx
@@ -14,6 +14,9 @@ function ForSale({ properties }) {
             {property.address}
           </Link>
         ))}
+            For Sale
+
+      
     </div>
   );
 }
--- src/components/OurBlog.jsx
+++ src/components/OurBlog.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import "./css/OurBlog.css"
 
 function OurBlog() {
   return (
-    <div>OurBlog</div>
-  )
+    <div className="blog-container">
+      <div className="blog-card1">Search</div>
+      <div className="blog-card2">
+        <div className="article">One</div>
+        <div className="article">Two</div>
+        <div className="article">Three</div>
+        <div className="article">Four</div>
+        <div className="article">Five</div>
+      </div>
+    </div>
+  );
 }
 
-export default OurBlog
+export default OurBlog;
