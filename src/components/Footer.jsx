7b76f53c89ac8759b882fd6d18438ac9affe0139
samjaygill <138401809+samjaygill@users.noreply.github.com>
2024-01-20 12:47:27 +0000

    added scroll to top button to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./css/Footer.css";
+import { Link } from "react-router-dom";
+import ScrollToTop from "react-scroll-to-top";
 
 function Footer() {
   return (
     <>
       <div className="footer-container">
-        <div className="card-1">
+        <div className="card1">
         <div className="footer1">
-          <p className="para-1">
+          <p className="para1">
             01563567865
             <br />
             0798637990
@@ -15,13 +17,20 @@ function Footer() {
             enquiries@meettheteam.com
           </p>
         </div>
-        <div className="footer2"></div>
+        <div className="footer2">
+        <p className="para2">
+            <Link to="/blog" className="para2">Our Blog</Link>
+            <br />
+            <Link to="/our-team" className="para2">Our Team</Link>
+
+          </p>
+        </div>
         </div>
 
         <div className="rights">
-          <p className="para-3">2024 Find My Next Home. All rights reserved.</p>
+          <p className="para3">2024 Find My Next Home. All rights reserved.</p>
         </div>
-        <div className="top"></div>
+        <div className="top"><ScrollToTop smooth/></div>
       </div>
     </>
   );
