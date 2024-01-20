c3cd1003d4f3c1f95960d0db19b1b35b7388817b
samjaygill <138401809+samjaygill@users.noreply.github.com>
2024-01-20 15:18:55 +0000

    updated footer and contact page

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import image from "./images/contactimg.png"
+import "./css/ContactUs.css"
 
 function ContactUs() {
   return (
-    <div>ContactUs</div>
+    <div className='contact-container'>
+      <div className='contact-card1'>
+        <img src={image} alt='contact center' className='contact-img'/>
+      </div>
+      <div className='contact-card2'> blah blah blah</div>
+    </div>
   )
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,18 +9,10 @@ function Footer() {
       <div className="footer-container">
         <div className="card1">
         <div className="footer1">
-          <p className="para1">
-            01563567865
-            <br />
-            0798637990
-            <br />
-            enquiries@meettheteam.com
-          </p>
+        <Link to="/blog" className="para1">Our Blog</Link>
         </div>
         <div className="footer2">
         <p className="para2">
-            <Link to="/blog" className="para2">Our Blog</Link>
-            <br />
             <Link to="/our-team" className="para2">Our Team</Link>
 
           </p>
