70df4300da985138b4231e4a0903e7a8dde5996b
samjaygill <138401809+samjaygill@users.noreply.github.com>
2024-01-20 14:50:32 +0000

    updates to blog

--- src/components/OurBlog.jsx
+++ src/components/OurBlog.jsx
@@ -1,10 +1,28 @@
-import React from "react";
-import "./css/OurBlog.css"
+import React, { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import "./css/OurBlog.css";
 
-function OurBlog() {
+function OurBlog({handleSearch}) {
+
+  const [input, setInput] = useState("");
+
+  const handleChange = (input) => {
+    setInput(input);
+    handleSearch(input);
+  };
+
   return (
     <div className="blog-container">
-      <div className="blog-card1">Search</div>
+      <div className="blog-card1">
+        <FaSearch id="search-icon" className="search-icon" />
+        <input
+          type="text"
+          className="input-search"
+          placeholder="Search..."
+          value={input}
+          onChange={(event) => handleChange(event.target.value)}
+        />
+      </div>
       <div className="blog-card2">
         <div className="article">One</div>
         <div className="article">Two</div>
