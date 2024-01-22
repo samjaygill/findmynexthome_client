fa349254055adde2726e0811c84b0ccec75761ee
samjaygill <138401809+samjaygill@users.noreply.github.com>
2024-01-22 16:09:15 +0000

    added sort and filter options

--- src/components/ForSale.jsx
+++ src/components/ForSale.jsx
@@ -1,40 +1,114 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "./css/ForSale.css";
 
 function ForSale({ properties }) {
+  const [sortBy, setSortBy] = useState("date");
+  const [sortOrder, setSortOrder] = useState("asc");
+  const [filterType, setFilterType] = useState("all");
+
+  const handleSort = (propertyKey) => {
+    if (sortBy === propertyKey) {
+      setSortOrder((prevOrder) => (prevOrder === "asc" ? "desc" : "asc"));
+    } else {
+      setSortBy(propertyKey);
+      setSortOrder("asc");
+    }
+  };
+
+  const handleFilter = (propertyType) => {
+    setFilterType(propertyType);
+  };
+
+  const sortedAndFilteredProperties = properties
+    .filter((property) => property.type === "sale")
+    .filter(
+      (property) => filterType === "all" || property.property === filterType
+    )
+    .sort((a, b) => {
+      const aValue = sortBy === "date" ? new Date(a[sortBy]) : a[sortBy];
+      const bValue = sortBy === "date" ? new Date(b[sortBy]) : b[sortBy];
+
+      return sortOrder === "asc" ? aValue - bValue : bValue - aValue;
+    });
+
   return (
     <div className="forsale-container">
-      <div className="options"></div>
+      <div className="options">
+        <div className="all">
+          <button onClick={() => handleFilter("all")} className="all-button">
+            Show All
+          </button>
+        </div>
+        <div className="date-price">
+          <p className="sort-title">Sort By:</p>
+          <button onClick={() => handleSort("date")} className="sort-button">
+            Date
+          </button>
+          <button onClick={() => handleSort("price")} className="sort-button">
+            Price
+          </button>
+        </div>
+        <div className="property-type">
+          <p className="type-title">Property Type:</p>
+          <div className="flex-box">
+            <button
+              onClick={() => handleFilter("Detached")}
+              className="type-button"
+            >
+              Detached
+            </button>
+            <button
+              onClick={() => handleFilter("Semi-Detached")}
+              className="type-button"
+            >
+              Semi-Detached
+            </button>
+            <button
+              onClick={() => handleFilter("Terraced")}
+              className="type-button"
+            >
+              Terraced
+            </button>
+            <button
+              onClick={() => handleFilter("Bungalow")}
+              className="type-button"
+            >
+              Bungalow
+            </button>
+            <button
+              onClick={() => handleFilter("Flat")}
+              className="type-button"
+            >
+              Flat
+            </button>
+          </div>
+        </div>
+      </div>
       <div className="forsale-cards">
-        {properties &&
-          properties
-            .filter((property) => property.type === "sale")
-            .map((property) => (
-              <Link
-                to={`/sale/${property.id}`}
-                className="property-card"
-                key={property.id}
-              >
-                <img
-                  src={property.imageUrls[0]}
-                  alt="front of property"
-                  className="fs-img-big"
-                />
-                <div className="property-details">
-                  <h5 className="property-title">{property.address}</h5>
-                  {property.property}
-                  <br />
-                  <i className="fa">&#xf236; </i> {property.bedrooms}{" "}
-                  <i className="fa">&#xf2cd; </i> {property.bathrooms}
-                  <p className="date">added on {property.date}</p>
-                  <p className="price">
-                    £{property.price.toLocaleString("en-GB")}
-                  </p>
-                </div>
-              </Link>
-            ))}
+        {sortedAndFilteredProperties.map((property) => (
+          <Link
+            to={`/sale/${property.id}`}
+            className="property-card"
+            key={property.id}
+          >
+            <img
+              src={property.imageUrls[0]}
+              alt="front of property"
+              className="fs-img-big"
+            />
+            <div className="property-details">
+              <h5 className="property-title">{property.address}</h5>
+              {property.property}
+              <br />
+              <i className="fa">&#xf236; </i> {property.bedrooms}{" "}
+              <i className="fa">&#xf2cd; </i> {property.bathrooms}
+              <p className="date">added on {property.date}</p>
+              <p className="price">£{property.price.toLocaleString("en-GB")}</p>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
