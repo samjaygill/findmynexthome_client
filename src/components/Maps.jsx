a2071e8dda024db8a24ebbc78b22af77e769e80f
samjaygill <138401809+samjaygill@users.noreply.github.com>
2024-02-12 15:30:21 +0000

    updated google map

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,22 +1,23 @@
-
-import { React} from "react";
-import {APIProvider, Map, Marker} from '@vis.gl/react-google-maps';
+import { React } from "react";
+import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
 import "./css/Map.css";
 
 function Maps() {
-    
-      return (
-         <div className="maps">
-        <APIProvider apiKey={"AIzaSyDHF4hFKBayBk1kDwTnOwpZfdN4cGseaKg"}>
+  return (
+    <div className="maps">
+      <APIProvider apiKey={"AIzaSyACsLcYxQS-rk5VOg1AhgD5bzeRU2Hbijw"}>
         <Map
           zoom={9}
-          center={{lat: 55.610963923751164, lng: -4.498863903753501}}
-          onLoad={() => console.log('Map loaded')}
-        />
-        <Marker position={{lat: 55.610963923751164, lng: -4.498863903753501}} />
+          center={{ lat: 55.610963923751164, lng: -4.498863903753501 }}
+          onLoad={() => console.log("Map loaded")}
+        >
+          <Marker
+            position={{ lat: 55.610963923751164, lng: -4.498863903753501 }}
+          />
+        </Map>
       </APIProvider>
-      </div>
-      );
-    };
+    </div>
+  );
+}
 
 export default Maps;
